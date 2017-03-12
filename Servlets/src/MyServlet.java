54c3bbfb3630478a0b499e05d48726ed9ad384ce
JoshC <Cun11002@byui.edu>
2017-03-11 19:54:17 -0700

    Both Servlets are combined into one file

---
+++ Servlets/src/MyServlet.java
@@ -0,0 +1,64 @@
+package servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name="basicServlet",urlPatterns={"/basicServlet"})
+public class basicServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+
+        try (PrintWriter writer = response.getWriter()) {
+
+
+            writer.println("<!DOCTYPE html><html>");
+            writer.println("<head>");
+            writer.println("<meta charset=\"UTF-8\" />");
+            writer.println("<title>ServletExample.java:doGet(): Servlet code!</title>");
+            writer.println("</head>");
+            writer.println("<body>");
+
+            writer.println("<h1>servlet.</h1>");
+
+            writer.println("</body>");
+            writer.println("</html>");
+        }
+    }
+}
+
+
+
+
+
+
+
+package servlet;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name="newServlet",urlPatterns={"/newServlet"})
+public class servlet extends HttpServlet {
+  public void doGet(HttpServletRequest request,
+                    HttpServletResponse response)
+      throws ServletException, IOException {
+      
+    PrintWriter out = response.getWriter();
+    out.println("Mic check, Mic check, Mic check");
+  }
+}
